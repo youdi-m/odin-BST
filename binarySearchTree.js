0beaa3d41a0e5a7ca509e540bfed62806a2c687a
youdi <mehdi.youssef97@gmail.com>
2026-05-07 11:09:43 -0400

    wrote all of the needed functions with short descriptions

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -4,6 +4,7 @@ class Node {
 		this.value = value;
 		this.left = null;
 		this.right = null;
+		
 	}
 }
 
@@ -28,5 +29,66 @@ class Tree {
 	buildTree(arr) {
 
 	}
+
+	// return true\false if value is\is not in the tree
+	includes(value) {
+
+	}
+
+	// insert a node with that value into the tree
+	// if the value exists do nothing
+	insert(value) {
+
+	}
+
+	// removes value from the tree
+	deleteItem(value) {
+
+	}
+
+	// accepts callback function
+	// traverses tree in BFS, passes the VALUE of the nodes as arguments
+	levelOrderForEach(callback) {
+
+	}
+
+	// accepts callback function
+	// traverses the tree from root - left - right
+	inOrderForEach(callback) {
+
+	}
+
+	// accepts callback function
+	// traverses the tree from left - root - right
+	preOrderForEach(callback) {
+
+	}
+
+	// accepts callback function
+	// traverses the tree from left - right - root
+	postOrderForEach(callback) {
+
+	}
+
+	// return the height of the node containing the given value
+	height(value) {
+
+	}
+
+	// return the depth of the node containing the given value
+	depth(value) {
+
+	}
+
+	// checks if tree is balanced
+	isBalanced() {
+
+	}
+
+	// rebalances an unbalanced tree
+	rebalance() {
+
+	}
+
 }
 
