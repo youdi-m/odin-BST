b82fe43372a5c3e5f496c1424847f877ebe8d00e
youdi <mehdi.yousef97@gmail.com>
2026-05-06 21:58:40 -0400

    started project

---
+++ binarySearchTree.js
@@ -0,0 +1,32 @@
+class Node {
+
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class Tree { 
+
+	constructor(arr) {
+		this.root = null;
+
+	}
+
+	// sorts the given array
+	sort(arr) {
+
+	}
+
+	// removes duplicates from the given array
+	removeDuplicates(arr) {
+
+	}
+
+	// turns the array into a balanced tree with node objects
+	buildTree(arr) {
+
+	}
+}
+
---
+++ main.js
@@ -0,0 +1,2 @@
+import {Tree} from './binarySearchTree.js';
+
